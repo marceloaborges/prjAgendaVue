a8945880d9dd88ed833c13023cbee46e760adf80
marceloaborges <marceloaborges@yahoo.com.br>
2024-02-17 09:16:38 -0300

    primeiras configurações vuejs

--- modernize/src/router/index.ts
+++ modernize/src/router/index.ts
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainRoutes from './MainRoutes';
 import AuthRoutes from './AuthRoutes';
 
-export default createRouter({
+export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        // {
-        //     path: '/:pathMatch(.*)*',
-        //     component: () => import('@/views/pages/Error404.vue')
-        // },
+        {
+            path: '/:pathMatch(.*)*',
+            component: () => import('@/views/pages/Error404.vue')
+        },
         MainRoutes,
         AuthRoutes
     ]
